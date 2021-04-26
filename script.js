0e37aa52256c350e63cc138bfdf1782d02925c81
kateryna <katerynamehezha@gmail.com>
2021-04-26 11:09:40 +0300

    task 2

--- script.js
+++ script.js
@@ -9,6 +9,25 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
   const oneTask = prompt('Один из последних просмотренных фильмов?', '')
   const twoTask = prompt('На сколько оценете его?', '')
-  personalMovieDB.movies[oneTask] = twoTask
+  if (
+    oneTask != null &&
+    twoTask != null &&
+    oneTask != '' &&
+    twoTask != '' &&
+    oneTask.length < 50
+  ) {
+    personalMovieDB.movies[oneTask] = twoTask
+  } else {
+    i--
+  }
+}
+if (personalMovieDB.count < 10) {
+  console.log('Просмотренно довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман')
+} else {
+  console.log('Произошла ошибка')
 }
 console.log(personalMovieDB)
